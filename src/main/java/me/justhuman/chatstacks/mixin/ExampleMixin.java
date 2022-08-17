54fb2d97ea31be1513f406c03c78d119c054da88
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2022-08-17 14:37:03 -0500

    Will this ever actually be finished

--- src/main/java/me/justhuman/chatstacks/mixin/ExampleMixin.java
+++ src/main/java/me/justhuman/chatstacks/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package me.justhuman.chatstacks.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import me.justhuman.chatstacks.ChatStacks;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		ChatStacks.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
